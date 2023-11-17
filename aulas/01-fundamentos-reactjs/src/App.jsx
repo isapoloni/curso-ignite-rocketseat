432b426bcb13d57079d861c47cb81059ae286f89
isapoloni <isabellapolonif@gmail.com>
2023-11-17 16:19:20 -0300

    starting to add javascript and logic to the Post

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,12 +1,51 @@
 
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import { Post }from './components/Post'
+import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
 import './global.css'
 
+
+
+//VictorGVC
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/isapoloni.png',
+      name: 'Isabella Poloni',
+      role: 'Front End Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-11-17 16:00:30')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/VictorGVC.png',
+      name: 'Victor Gabriel',
+      role: 'Back End Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-11-22 10:25:30')
+  },
+];
+
+//itreação
+
+//forEach retorna undefined
 
 export default function App() {
 
@@ -17,14 +56,15 @@ export default function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Isabella Poloni"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, suscipit architecto totam eveniet qui soluta placeat sint at, doloremque mollitia rerum atque nostrum nobis beatae optio sit. Et, nemo adipisci."
-          />
-          <Post
-            author="Post teste"
-            content="Um novo post muito legal"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
